5170576a5392c8543d3bc20e2b510d26777c940e
Thiago Marinho <tgmarinho@gmail.com>
2020-04-17 16:32:51 -0400

    validate route delete (id)

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -8,7 +8,19 @@ interface Request {
 }
 
 class DeleteTransactionService {
+  private isValid(id: string): boolean {
+    const pattern = new RegExp(
+      /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i,
+    );
+
+    return pattern.test(id);
+  }
+
   public async execute({ id }: Request): Promise<void> {
+    if (!this.isValid(id)) {
+      throw new AppError('Id/Route is not valid');
+    }
+
     const transactionRepository = getCustomRepository(TransactionRepository);
     const transaction = await transactionRepository.findOne(id);
     if (!transaction) {
