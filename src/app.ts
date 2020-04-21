45c70c1eee0ff9b3e23c41ac8c17cfe715f2efb1
Thiago Marinho <tgmarinho@gmail.com>
2020-04-21 17:04:19 -0400

    preparando para consumir os dados da API no frontend

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 import 'dotenv/config';
-
+import cors from 'cors';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 
@@ -13,6 +13,7 @@ createConnection();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
